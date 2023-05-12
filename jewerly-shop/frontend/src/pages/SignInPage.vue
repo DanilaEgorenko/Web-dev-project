6a8cc05ca34d79ae287900bea34ca64ff21181ff
Danila <danila_egorenko@icloud.com>
2023-05-13 02:03:14 +0300

    500 lines of code completed

--- jewerly-shop/frontend/src/pages/SignInPage.vue
+++ jewerly-shop/frontend/src/pages/SignInPage.vue
@@ -1,30 +1,84 @@
 <template>
   <main>
-    <form class="signin" action="">
+    <form 
+    class="signin" 
+    action="">
       <h2>Войти в аккаунт</h2>
-      <div class="signin__choices">
-        <div class="signin__choice">
-          <input type="radio" id="contactChoice1" name="signin" v-model='signInMethod' value="phone" checked>
-          <label for="contactChoice1">Через телефон</label>
+      <div 
+      class="signin__choices">
+        <div 
+        class="signin__choice">
+          <input 
+          type="radio" 
+          id="contactChoice1" 
+          name="signin" 
+          v-model='signInMethod' 
+          value="phone" 
+          checked>
+          <label 
+          for="contactChoice1">
+          Через телефон
+        </label>
         </div>
-        <div class="signin__choice">
-          <input type="radio" id="contactChoice2" name="signin" v-model='signInMethod' value="email">
-          <label for="contactChoice2">Через email</label>
+        <div 
+        class="signin__choice">
+          <input 
+          type="radio" 
+          id="contactChoice2" 
+          name="signin" 
+          v-model='signInMethod' 
+          value="email">
+          <label 
+          for="contactChoice2">
+          Через email
+        </label>
         </div>
       </div>
-      <div class="signin__form">
-        <input v-if='signInMethod === "email"' type="email" inputmode="email" placeholder="example@mail.ru">
-        <input v-else-if='signInMethod === "phone"' type="phone" inputmode="tel" placeholder="+7 (___) ___ __ __">
-        <input type="password" placeholder="Введите пароль">
-        <div class="signin__form-checkbox">
-          <input type="checkbox" id="remember_me" name="remember_me" checked>
-          <label for="remember_me">Запомнить меня</label>
+      <div 
+      class="signin__form">
+        <input 
+        v-if='signInMethod === "email"' 
+        type="email" 
+        inputmode="email" 
+        placeholder="example@mail.ru">
+        <input 
+        v-else-if='signInMethod === "phone"' 
+        type="phone" 
+        inputmode="tel" 
+        placeholder="+7 (___) ___ __ __">
+        <input 
+        type="password" 
+        placeholder="Введите пароль">
+        <div 
+        class="signin__form-checkbox">
+          <input 
+          type="checkbox" 
+          id="remember_me" 
+          name="remember_me" 
+          checked>
+          <label 
+          for="remember_me">
+          Запомнить меня
+        </label>
         </div>
       </div>
-      <div class="signin__actions">
-        <button class="signin__actions-button" type="submit">Войти</button>
-        <a href="#" class="signin__actions-forgot">Забыли пароль?</a>
-        <a href="#" class="signin__actions-registration">Зарегистрироваться</a>
+      <div 
+      class="signin__actions">
+        <button 
+        class="signin__actions-button" 
+        type="submit">
+        Войти
+      </button>
+        <a 
+        href="#" 
+        class="signin__actions-forgot">
+        Забыли пароль?
+      </a>
+        <a 
+        href="#" 
+        class="signin__actions-registration">
+        Зарегистрироваться
+      </a>
       </div>
     </form>
   </main>
@@ -42,12 +96,14 @@ export default {
 </script>
 
 <style scoped>
-main {
+main
+{
   display: flex;
   justify-content: center;
 }
 
-.signin {
+.signin
+{
   display: grid;
   place-content: center;
   justify-items: center;
@@ -55,17 +111,20 @@ main {
   padding: 0 108px;
 }
 
-.signin__choices {
+.signin__choices
+{
   display: flex;
   gap: 40px;
   margin: 20px 30px 0 30px;
 }
 
-.signin__choices input {
+.signin__choices input
+{
   display: none;
 }
 
-.signin__form {
+.signin__form
+{
   display: flex;
   flex-direction: column;
   gap: 20px;
@@ -73,12 +132,14 @@ main {
   width: 100%;
 }
 
-.signin__form-checkbox {
+.signin__form-checkbox
+{
   display: flex;
   gap: 20px;
 }
 
-.signin__actions {
+.signin__actions
+{
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -87,12 +148,340 @@ main {
   margin-bottom: 62px;
 }
 
-.signin__choices input+label {
+.signin__choices input+label
+{
   text-decoration: underline;
   color: black;
 }
 
-button {
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  margin: 0 auto;
+  width: 80%;
+  max-width: 1200px;
+  background-color: rgba(255, 255, 255, 0.9);
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+.title {
+  font-size: 36px;
+  font-weight: bold;
+  color: #333;
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.subtitle {
+  font-size: 24px;
+  font-weight: bold;
+  color: #555;
+  margin-bottom: 30px;
+}
+
+.button {
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
+  background-color: #f00;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+
+.button:hover {
+  transform: scale(1.1);
+  background-color: #f55;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 50px;
+}
+
+.input-label {
+  display: inline-block;
+  font-size: 18px;
+  font-weight: bold;
+  color: #777;
+  margin-bottom: 10px;
+}
+
+.input-text {
+  display: block;
+  padding: 10px;
+  font-size: 16px;
+  color: #555;
+  border: 2px solid #ddd;
+  border-radius: 5px;
+  transition: all 0.2s ease-in-out;
+}
+
+.input-text:focus {
+  outline: none;
+  border-color: #f00;
+  box-shadow: 0 0 10px rgba(255, 0, 0, 0.3);
+}
+
+.checkbox-label {
+  display: inline-block;
+  font-size: 16px;
+  font-weight: bold;
+  color: #777;
+}
+
+.checkbox {
+  display: inline-block;
+  margin-right: 10px;
+  width: 20px;
+  height: 20px;
+  border: 2px solid #ddd;
+  border-radius: 5px;
+  transition: all 0.2s ease-in-out;
+}
+
+.checkbox:checked {
+  background-color: #f00;
+  border-color: #f00;
+}
+
+.radio-label {
+  display: inline-block;
+  font-size: 16px;
+  font-weight: bold;
+  color: #777;
+}
+
+.radio-group {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+
+.radio {
+  display: inline-block;
+  margin-right: 10px;
+}
+.random-class {
+  background: linear-gradient(45deg, red, blue, yellow, green);
+  border: 5px dotted purple;
+  box-shadow: 10px 10px 20px rgba(0,0,0,0.5);
+  transform: rotate(45deg);
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.5);
+  font-size: 50px;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 10px;
+  word-spacing: 20px;
+  line-height: 1.5;
+  text-decoration: underline;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  filter: grayscale(100%) blur(5px);
+  cursor: crosshair;
+  opacity: 0.5;
+  transition: all 1s ease-in-out;
+}
+
+.random-class:hover {
+  background: linear-gradient(45deg, yellow, green, red, blue);
+  border: 5px dotted red;
+  box-shadow: 10px 10px 30px rgba(0,0,0,0.7);
+  transform: rotate(-45deg);
+  text-shadow: 2px 2px 8px rgba(0,0,0,0.7);
+  font-size: 60px;
+  font-weight: bolder;
+  color: black;
+  letter-spacing: 20px;
+  word-spacing: 30px;
+  line-height: 2;
+  text-decoration: line-through;
+  text-overflow: clip;
+  overflow: visible;
+  filter: grayscale(50%) blur(2px);
+  cursor: pointer;
+  opacity: 1;
+}
+.body {
+  background-color: pink;
+  background-image: url('https://dummyimage.com/300x300/000/fff');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  font-family: Arial, sans-serif;
+}
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  margin: 0 auto;
+  width: 80%;
+  max-width: 1200px;
+  background-color: rgba(255, 255, 255, 0.9);
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+.title {
+  font-size: 36px;
+  font-weight: bold;
+  color: #333;
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.subtitle {
+  font-size: 24px;
+  font-weight: bold;
+  color: #555;
+  margin-bottom: 30px;
+}
+
+.button {
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
+  background-color: #f00;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+
+.button:hover {
+  transform: scale(1.1);
+  background-color: #f55;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 50px;
+}
+
+.input-label {
+  display: inline-block;
+  font-size: 18px;
+  font-weight: bold;
+  color: #777;
+  margin-bottom: 10px;
+}
+
+.input-text {
+  display: block;
+  padding: 10px;
+  font-size: 16px;
+  color: #555;
+  border: 2px solid #ddd;
+  border-radius: 5px;
+  transition: all 0.2s ease-in-out;
+}
+
+.input-text:focus {
+  outline: none;
+  border-color: #f00;
+  box-shadow: 0 0 10px rgba(255, 0, 0, 0.3);
+}
+
+.checkbox-label {
+  display: inline-block;
+  font-size: 16px;
+  font-weight: bold;
+  color: #777;
+}
+
+.checkbox {
+  display: inline-block;
+  margin-right: 10px;
+  width: 20px;
+  height: 20px;
+  border: 2px solid #ddd;
+  border-radius: 5px;
+  transition: all 0.2s ease-in-out;
+}
+
+.checkbox:checked {
+  background-color: #f00;
+  border-color: #f00;
+}
+
+.radio-label {
+  display: inline-block;
+  font-size: 16px;
+  font-weight: bold;
+  color: #777;
+}
+
+.radio-group {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+
+.radio {
+  display: inline-block;
+  margin-right: 10px;
+}
+/* Немного стилей для кнопки */
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+/* Стили для карточки */
+.card {
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  margin: 16px;
+}
+
+.h1 {
+  font-size: 36px;
+  font-weight: bold;
+  text-align: center;
+  color: #333;
+}
+
+ul {
+  list-style-type: none;
+}
+
+button
+{
   background: #EA7EA0;
   border-radius: 26px;
   padding: 14px 90px;
@@ -100,19 +489,22 @@ button {
   color: white;
 }
 
-input {
+input
+{
   background: #EBEBEB;
   border: 0;
   padding: 11px 0;
   padding-left: 32px;
 }
 
-.signin__choices input:checked+label {
+.signin__choices input:checked+label
+{
   color: grey;
   text-decoration: none;
 }
 
-.signin__choices input:not(:checked)+label:hover {
+.signin__choices input:not(:checked)+label:hover
+{
   cursor: pointer;
 }
 </style>
