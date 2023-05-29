a84f02932e98d4bdfaf31b0115de0a2a03100ada
Falcon <mr.sokol5253@gmail.com>
2023-05-29 23:57:31 +0300

    changed good model

--- jewerly-shop/backend/apps/goods/models.py
+++ jewerly-shop/backend/apps/goods/models.py
@@ -7,10 +7,12 @@ from django.db import models
 
 class Good(models.Model):
     """Модель товара"""
-    name = models.CharField(verbose_name="Название", max_length=64)
+    title = models.CharField(verbose_name="Название", max_length=64)
     price = models.DecimalField(verbose_name="Цена", max_digits=9, decimal_places=2, default=0.00)
+    material = models.CharField(verbose_name="Материал", max_length=32, default="Silver")
+    img_path = models.CharField(verbose_name="Путь к изображению", max_length=9999, default="/default-img.png")
+    color = models.CharField(verbose_name="Цвет", max_length=32, default="Серый")
     category = None
-    alloy = models.CharField(verbose_name="Сплав", max_length=64, default="copper")
 
     class Meta:
         verbose_name = "Good"
--- jewerly-shop/backend/apps/goods/serializers.py
+++ jewerly-shop/backend/apps/goods/serializers.py
@@ -5,7 +5,9 @@ class GoodSerializer(serializers.Serializer):
     """
     All fields of Goods model that can be accessed from api
     """
-    name = serializers.CharField(max_length=64)
+    title = serializers.CharField(max_length=64)
     price = serializers.DecimalField(max_digits=9, decimal_places=2)
     category = None
-    alloy = serializers.CharField(max_length=64)
+    material = serializers.CharField(max_length=64)
+    src = serializers.CharField(max_length=9999)
+    color = serializers.CharField(max_length=32)
