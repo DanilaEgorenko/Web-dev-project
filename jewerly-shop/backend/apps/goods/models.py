6b72118923996d75375925a8bb42f552a23356de
Falcon <mr.sokol5253@gmail.com>
2023-05-31 12:03:51 +0300

    added static files

--- jewerly-shop/backend/apps/goods/models.py
+++ jewerly-shop/backend/apps/goods/models.py
@@ -10,7 +10,7 @@ class Good(models.Model):
     title = models.CharField(verbose_name="Название", max_length=64)
     price = models.DecimalField(verbose_name="Цена", max_digits=9, decimal_places=2, default=0.00)
     material = models.CharField(verbose_name="Материал", max_length=32, default="Silver")
-    img_path = models.CharField(verbose_name="Путь к изображению", max_length=9999, default="/default-img.png")
+    img = models.ImageField(verbose_name="Изоюражение", max_length=9999, default=None, null=True, upload_to ='static/img/')
     color = models.CharField(verbose_name="Цвет", max_length=32, default="Серый")
     category = None
 
@@ -19,4 +19,4 @@ class Good(models.Model):
         verbose_name_plural = "Goods"
 
     def __str__(self):
-        return str(self.name)
+        return str(self.title)
--- jewerly-shop/backend/apps/goods/serializers.py
+++ jewerly-shop/backend/apps/goods/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Good
 
 
 class GoodSerializer(serializers.Serializer):
@@ -9,5 +10,12 @@ class GoodSerializer(serializers.Serializer):
     price = serializers.DecimalField(max_digits=9, decimal_places=2)
     category = None
     material = serializers.CharField(max_length=64)
-    src = serializers.CharField(max_length=9999)
+    src = serializers.SerializerMethodField('get_image_url')
     color = serializers.CharField(max_length=32)
+    
+    class Meta:
+        model = Good
+        fields = ('title', 'price', 'src', 'category', 'material', 'color')
+
+    def get_image_url(self, obj):
+        return obj.img.url
